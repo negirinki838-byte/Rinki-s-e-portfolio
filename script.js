b0512f02eda1f537b91438f65bc3e9dadbccb565
Rinki Negi <negirinki838@gmail.com>
2026-05-04 21:37:45 +0530

    Refactor script.js to add particles and typing effect

--- script.js
+++ script.js
@@ -18,3 +18,36 @@ function openPDF(file) {
 function closePDF() {
   document.getElementById("pdfModal").style.display = "none";
 }
+
+// PARTICLES
+const container = document.querySelector(".particles");
+
+for(let i=0;i<40;i++){
+  let span = document.createElement("span");
+  span.style.left = Math.random()*100+"vw";
+  span.style.animationDuration = (5+Math.random()*5)+"s";
+  container.appendChild(span);
+}
+
+// TYPING EFFECT
+const text = "Hi I'm Rinki 🌸";
+let i = 0;
+
+function typing() {
+  if (i < text.length) {
+    document.querySelector(".typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typing, 80);
+  }
+}
+typing();
+
+// CURSOR GLOW
+const cursor = document.createElement("div");
+cursor.classList.add("cursor");
+document.body.appendChild(cursor);
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
